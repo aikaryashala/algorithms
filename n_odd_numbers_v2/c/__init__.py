fa25793f43896f15db3b31a22b51e453f899d185
Rohini Kumar Barla <rohinibarla@aikaryashala.com>
2025-11-20 14:13:52 +0530

    Making a copy of test cases for V2 of N even numbers and N odd numbers

---
+++ n_odd_numbers_v2/c/__init__.py
@@ -0,0 +1,44 @@
+import check50
+import check50.c
+
+EXECUTABLE = "./n_odd_numbers"
+
+# Include files at module level (executed once)
+TEST_FILES = [
+    "../tests/input1.txt", "../tests/expected_output1.txt",
+    "../tests/input2.txt", "../tests/expected_output2.txt",
+    "../tests/input3.txt", "../tests/expected_output3.txt"
+]
+
+@check50.check()
+def exists_and_compiles():
+    """n_odd_numbers.c exists"""
+    check50.exists("n_odd_numbers.c")
+
+    """n_odd_numbers.c compiles"""
+    check50.c.compile("n_odd_numbers.c", lcs50=False)
+
+    include_test_files()
+
+@check50.check(exists_and_compiles)
+def n_odd_numbers_test1():
+    test_input_output("input1.txt", "expected_output1.txt");
+
+@check50.check(exists_and_compiles)
+def n_odd_numbers_test2():
+    test_input_output("input2.txt", "expected_output2.txt");
+
+@check50.check(exists_and_compiles)
+def n_odd_numbers_test3():
+    test_input_output("input3.txt", "expected_output3.txt");
+
+
+# Helper functions
+def test_input_output(input_file, output_file):
+    """A function to test a single input/output pair"""
+    check50.run(EXECUTABLE).stdin(open(input_file).read(), prompt=False).stdout(open(output_file).read(), regex=False).exit()
+
+def include_test_files():
+    for test_file in TEST_FILES:
+        check50.include(test_file)
+
