794d031647b9f2f408ed1f123ac8cf1c3320fba0
Rohini Kumar Barla <rohinibarla@aikaryashala.com>
2025-11-20 14:56:11 +0530

    Tests for Sum of N natural numbers

---
+++ sum_of_n_natural_numbers/c/__init__.py
@@ -0,0 +1,40 @@
+import check50
+import check50.c
+
+EXECUTABLE = "./sum_of_n_natural_numbers"
+
+TEST_FILES = [
+    "../tests/input1.txt", "../tests/expected_output1.txt",
+    "../tests/input2.txt", "../tests/expected_output2.txt",
+    "../tests/input3.txt", "../tests/expected_output3.txt"
+]
+
+@check50.check()
+def exists_and_compiles():
+    """sum_of_n_natural_numbers.c exists"""
+    check50.exists("sum_of_n_natural_numbers.c")
+
+    """sum_of_n_natural_numbers.c compiles"""
+    check50.c.compile("sum_of_n_natural_numbers.c", lcs50=False)
+
+    include_test_files()
+
+@check50.check(exists_and_compiles)
+def sum_of_n_natural_numbers_test1():
+    test_input_output("input1.txt", "expected_output1.txt")
+
+@check50.check(exists_and_compiles)
+def sum_of_n_natural_numbers_test2():
+    test_input_output("input2.txt", "expected_output2.txt")
+
+@check50.check(exists_and_compiles)
+def sum_of_n_natural_numbers_test3():
+    test_input_output("input3.txt", "expected_output3.txt")
+
+def test_input_output(input_file, output_file):
+    """A function to test a single input/output pair"""
+    check50.run(EXECUTABLE).stdin(open(input_file).read(), prompt=False).stdout(open(output_file).read(), regex=False).exit()
+
+def include_test_files():
+    for test_file in TEST_FILES:
+        check50.include(test_file)
