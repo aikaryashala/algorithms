1b2b7ab88bf51c1efc089a61504d954a5f9e0569
Rohini Kumar Barla <rohinibarla@aikaryashala.com>
2025-11-21 11:04:21 +0530

    Tests for Max of two numbers V2

---
+++ max_of_2_v2/c/__init__.py
@@ -0,0 +1,35 @@
+import check50
+import check50.c
+
+EXECUTABLE = "./max_of_2_v2"
+
+TEST_FILES = [
+    "../tests/input1.txt", "../tests/expected_output1.txt",
+    "../tests/input2.txt", "../tests/expected_output2.txt"
+]
+
+@check50.check()
+def exists_and_compiles():
+    """max_of_2_v2.c exists"""
+    check50.exists("max_of_2_v2.c")
+
+    """max_of_2_v2.c compiles"""
+    check50.c.compile("max_of_2_v2.c", lcs50=False)
+
+    include_test_files()
+
+@check50.check(exists_and_compiles)
+def max_of_2_test1():
+    test_input_output("input1.txt", "expected_output1.txt")
+
+@check50.check(exists_and_compiles)
+def max_of_2_test2():
+    test_input_output("input2.txt", "expected_output2.txt")
+
+def test_input_output(input_file, output_file):
+    """A function to test a single input/output pair"""
+    check50.run(EXECUTABLE).stdin(open(input_file).read(), prompt=False).stdout(open(output_file).read(), regex=False).exit()
+
+def include_test_files():
+    for test_file in TEST_FILES:
+        check50.include(test_file)
