e5f0ad86f2969a56cd7f12fa61410e86a3339860
Rohini Kumar Barla <rohinibarla@aikaryashala.com>
2025-11-21 11:47:52 +0530

    Tests for Maximum of three numbers V2

---
+++ max_of_3_v2/py/__init__.py
@@ -0,0 +1,50 @@
+import check50
+
+EXECUTABLE = "python3 max_of_3_v2.py"
+
+TEST_FILES = [
+    "../tests/input1.txt", "../tests/expected_output1.txt",
+    "../tests/input2.txt", "../tests/expected_output2.txt",
+    "../tests/input3.txt", "../tests/expected_output3.txt",
+    "../tests/input4.txt", "../tests/expected_output4.txt",
+    "../tests/input5.txt", "../tests/expected_output5.txt",
+    "../tests/input6.txt", "../tests/expected_output6.txt"
+]
+
+@check50.check()
+def exists_and_compiles():
+    """max_of_3_v2.py exists"""
+    check50.exists("max_of_3_v2.py")
+    include_test_files()
+
+@check50.check(exists_and_compiles)
+def max_of_3_v2_test1():
+    test_input_output("input1.txt", "expected_output1.txt")
+
+@check50.check(exists_and_compiles)
+def max_of_3_v2_test2():
+    test_input_output("input2.txt", "expected_output2.txt")
+
+@check50.check(exists_and_compiles)
+def max_of_3_v2_test3():
+    test_input_output("input3.txt", "expected_output3.txt")
+
+@check50.check(exists_and_compiles)
+def max_of_3_v2_test4():
+    test_input_output("input4.txt", "expected_output4.txt")
+
+@check50.check(exists_and_compiles)
+def max_of_3_v2_test5():
+    test_input_output("input5.txt", "expected_output5.txt")
+
+@check50.check(exists_and_compiles)
+def max_of_3_v2_test6():
+    test_input_output("input6.txt", "expected_output6.txt")
+
+def test_input_output(input_file, output_file):
+    """A function to test a single input/output pair"""
+    check50.run(EXECUTABLE).stdin(open(input_file).read(), prompt=False).stdout(open(output_file).read(), regex=False).exit()
+
+def include_test_files():
+    for test_file in TEST_FILES:
+        check50.include(test_file)
